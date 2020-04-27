9936a39e1edb4d5b2a6767f478fc2d4ebfe3d3aa
konstantin <darkkon@mail.ru>
2020-04-27 17:38:45 +0300

    fix camera fallow after restart

--- Assets/Scripts/CameraFallowTo.cs
+++ Assets/Scripts/CameraFallowTo.cs
@@ -13,30 +13,28 @@ public class CameraFallowTo : MonoBehaviour {
         _cam = GetComponent<Camera>();
         _offset = transform.position - _target.position;
         GameEvent.OnChangeStage += OnGameStageChanged;
-        GameEvent.OnReady += OnGameReady;
     }
 
     private void OnDestroy() {
         GameEvent.OnChangeStage -= OnGameStageChanged;
-        GameEvent.OnReady -= OnGameReady;
     }
 
     private void OnGameStageChanged() {
         this.enabled = GameEvent.Current == GameStage.READY || GameEvent.Current == GameStage.START;
     }
-
-    private void OnGameReady() {
-        transform.position = _target.position + _offset;
-    }
-
+    
     private void LateUpdate() {
         var targetPos = _target.position;
         var camPosForCenter = targetPos + _offset;
-        var horizontalOffset = Vector3.Project(camPosForCenter - transform.position, transform.right);
-        
-        var viewPos = _cam.WorldToViewportPoint(targetPos);
-        var horizontalMagnet = _curveMagnetToCenter.Evaluate(Mathf.Abs(viewPos.x - 0.5f) * 2f);
+        if (GameEvent.Current == GameStage.START) {
+            var horizontalOffset = Vector3.Project(camPosForCenter - transform.position, transform.right);
 
-        transform.position = camPosForCenter - (1f - Time.deltaTime * horizontalMagnet) *  horizontalOffset;
+            var viewPos = _cam.WorldToViewportPoint(targetPos);
+            var horizontalMagnet = _curveMagnetToCenter.Evaluate(Mathf.Abs(viewPos.x - 0.5f) * 2f);
+
+            transform.position = camPosForCenter - (1f - Time.deltaTime * horizontalMagnet) * horizontalOffset;
+        } else {
+            transform.position = camPosForCenter;
+        }
     }
 }
