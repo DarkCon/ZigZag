b6929bc06d341b1c7c62c13a96532c698a8bffb6
konstantin <darkkon@mail.ru>
2020-04-27 15:36:04 +0300

    fix bonus may spawn in first segment

--- Assets/Scripts/Map/MapGeneratorRandomBonus.cs
+++ Assets/Scripts/Map/MapGeneratorRandomBonus.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 namespace Map {
     public class MapGeneratorRandomBonus : MapGeneratorAbstract {
+        private bool _isFirstSegment;
+        
+        public override void ResetState() {
+            base.ResetState();
+            _isFirstSegment = true;
+        }
+
         protected override void GenerateBonus(ref Segment segment) {
-            segment.hasBonus = Random.Range(0, BonusPeriod) < 1;
-            if (segment.hasBonus) {
-                segment.bonusPos = GetRandomPosWithOffset(segment.size, MinBonusOffsetFromEdge);
+            if (_isFirstSegment) {
+                _isFirstSegment = false;
+            } else {
+                segment.hasBonus = Random.Range(0, BonusPeriod) < 1;
+                if (segment.hasBonus) {
+                    segment.bonusPos = GetRandomPosWithOffset(segment.size, MinBonusOffsetFromEdge);
+                }
             }
         }
     }
